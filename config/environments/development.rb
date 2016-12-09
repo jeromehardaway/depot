ab40d7476c42c7b5f8d9284ce190c26db0c7fe34
Jerome Hardaway <jerome@vetswhocode.io>
2016-12-08 19:40:16 -0600

    Email Template

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,6 +16,7 @@ Depot::Application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
 
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -26,4 +27,8 @@ Depot::Application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+end
+
+Depot::Application.configure do
+  config.action_mailer.delivery_method = :test
 end
