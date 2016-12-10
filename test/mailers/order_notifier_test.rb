694cffb92204f88d64f39bc54cc517a70918af5d
Jerome Hardaway <jerome@vetswhocode.io>
2016-12-10 08:16:43 -0600

    Email Testing complete

--- test/mailers/order_notifier_test.rb
+++ test/mailers/order_notifier_test.rb
@@ -3,18 +3,18 @@ require 'test_helper'
 class OrderNotifierTest < ActionMailer::TestCase
   test "recieved" do
     mail = OrderNotifier.recieved(orders(:one))
-    assert_equal "Recieved", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_equal "Store Order Confirmation", mail.subject
+    assert_equal ["jerome@vetswhocode.io"], mail.to
+    assert_equal ["hello@vetswhocode.io"], mail.from
+    assert_match /1 x Programming Ruby 1.9/, mail.body.encoded
   end
 
   test "shipped" do
-    mail = OrderNotifier.shipped
-    assert_equal "Shipped", mail.subject
-    assert_equal ["to@example.org"], mail.to
-    assert_equal ["from@example.com"], mail.from
-    assert_match "Hi", mail.body.encoded
+    mail = OrderNotifier.shipped(orders(:one))
+    assert_equal "Store Order Shipped", mail.subject
+    assert_equal ["jerome@vetswhocode.io"], mail.to
+    assert_equal ["hello@vetswhocode.io"], mail.from
+    assert_match /<td>l&times;<\/td>\s*<td>Programming Ruby 1.9<\/td>/, mail.body.encoded
   end
 
 end
