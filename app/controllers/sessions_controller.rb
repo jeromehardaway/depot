d3247f17129f571f55f9714718ed642e234f6da3
Jerome Hardaway <jerome@vetswhocode.io>
2016-12-12 14:43:20 -0600

    Loggin in complete

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,20 @@
 class SessionsController < ApplicationController
+  skip_before_filter :authorize
   def new
   end
 
   def create
+    user = Usere.find_by_name(params[:name])
+    if user and user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to admin_url
+    else
+      redirect_to login_url, alert: "Invalied user/password comboniation"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to store_url, notice: "Logged out"
   end
 end
